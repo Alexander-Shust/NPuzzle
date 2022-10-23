17f9dbca962f0cb652c1a0059e427a1038e2b06d
Alexander Shust <alexander.shust@developersunknown.com>
2022-10-24 00:49:46 +0300

    Swapping numbered pieces

--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -17,23 +17,114 @@ public class PuzzleManager : MonoBehaviour
     private float _boardWidth;
     private float _boardHeight;
     private float _pieceSize;
-    
+
+    private GameObject[] _pieces;
+    private int _emptyPosition;
+
     private void Start()
     {
         var boardLocalScale = _board.localScale;
         _boardWidth = boardLocalScale.x;
         _boardHeight = boardLocalScale.z;
-        _pieceSize = (Mathf.Min(_boardWidth, _boardHeight) - _spacing * (_boardSize - 1)) / _boardSize;
-        var fillOrigin = new Vector3((_pieceSize -_boardWidth) / 2, 0.0f, (_pieceSize -_boardHeight) / 2);
+        var puzzle = GeneratePuzzle(_boardSize);
+        InitBoard(_boardSize, puzzle);
+        Down();
+        Right();
+        Down();
+        Left();
+    }
+
+    public void InitBoard(int size, int[,] pieces)
+    {
+        _pieces = new GameObject[size * size];
+        _pieceSize = (Mathf.Min(_boardWidth, _boardHeight) - _spacing * (size - 1)) / size;
+        var fontSize = 36 / size;
+        var fillOrigin = new Vector3((_pieceSize -_boardWidth) / 2, 0.0f, (_boardHeight - _pieceSize) / 2);
         
-        for (var i = 0; i < _boardSize; ++i)
+        for (var i = 0; i < size; ++i)
         {
-            for (var j = 0; j < _boardSize; ++j)
+            for (var j = 0; j < size; ++j)
             {
-                var piecePosition = fillOrigin + new Vector3(i * (_pieceSize + _spacing), 1, j * (_pieceSize + _spacing));
+                if (pieces[i, j] == 0)
+                {
+                    _emptyPosition = i * size + j;
+                    continue;
+                }
+                
+                var piecePosition = fillOrigin + new Vector3(j * (_pieceSize + _spacing), 1, -i * (_pieceSize + _spacing));
                 var pieceGo = Instantiate(_piecePrefab, piecePosition, Quaternion.identity, _board);
                 pieceGo.transform.localScale *= _pieceSize / Mathf.Min(_boardHeight, _boardWidth);
+                _pieces[i * size + j] = pieceGo;
+
+                var piece = pieceGo.GetComponent<PuzzlePiece>();
+                piece.SetNumber(pieces[i, j]);
+                piece.SetFontSize(fontSize);
             }
         }
+    }
+
+    private void Up()
+    {
+        var target = _emptyPosition - _boardSize;
+        if (target < 0) return;
+        
+        var pieceGo = _pieces[target];
+        var position = pieceGo.transform.position;
+        position.z -= _pieceSize + _spacing;
+        pieceGo.transform.position = position;
+        SwapWithEmpty(target);
+    }
+
+    private void Down()
+    {
+        var target = _emptyPosition + _boardSize;
+        if (target >= _boardSize * _boardSize) return;
+        
+        var pieceGo = _pieces[target];
+        var position = pieceGo.transform.position;
+        position.z += _pieceSize + _spacing;
+        pieceGo.transform.position = position;
+        SwapWithEmpty(target);
+    }
+
+    private void Left()
+    {
+        if (_emptyPosition % _boardSize == 0) return;
+        var target = _emptyPosition - 1;
+        
+        var pieceGo = _pieces[target];
+        var position = pieceGo.transform.position;
+        position.x += _pieceSize + _spacing;
+        pieceGo.transform.position = position;
+        SwapWithEmpty(target);
+    }
+
+    private void Right()
+    {
+        var target = _emptyPosition + 1;
+        if (target % _boardSize == 0) return;
+        
+        var pieceGo = _pieces[target];
+        var position = pieceGo.transform.position;
+        position.x -= _pieceSize + _spacing;
+        pieceGo.transform.position = position;
+        SwapWithEmpty(target);
+    }
+
+    private void SwapWithEmpty(int target)
+    {
+        _pieces[_emptyPosition] = _pieces[target];
+        _pieces[target] = null;
+        _emptyPosition = target;
+    }
+
+    public int[,] GeneratePuzzle(int size)
+    {
+        var puzzle = new int[size, size];
+        for (var i = 0; i < size * size; ++i)
+        {
+            puzzle[i / size, i % size] = i;
+        }
+        return puzzle;
     }
 }
--- Assets/Scripts/PuzzlePiece.cs
+++ Assets/Scripts/PuzzlePiece.cs
@@ -1,6 +1,18 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class PuzzlePiece : MonoBehaviour
 {
-    
+    [SerializeField]
+    private TMP_Text _numberText;
+
+    public void SetNumber(int number)
+    {
+        _numberText.text = number.ToString();
+    }
+
+    public void SetFontSize(float fontSize)
+    {
+        _numberText.fontSize = fontSize;
+    }
 }
